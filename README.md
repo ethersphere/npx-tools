3488ac787de626b88a6c2327ff52d1714c991587
Cafe137 <xcafe137@gmail.com>
2024-07-25 00:55:28 +0200

    feat: add bee-yaml

---
+++ README.md
@@ -0,0 +1,8 @@
+# bee-yaml
+
+Prints an ultra-light Bee configuration file to stdout.
+
+```
+mkdir -p /etc/bee
+npx bee-yaml > /etc/bee/bee.yaml
+```
---
+++ bee-yaml/index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+console.log(`config: /etc/bee/bee.yaml
+cors-allowed-origins: *
+data-dir: /var/lib/bee
+api-addr: 127.0.0.1:1633
+debug-api-addr: 127.0.0.1:1635
+debug-api-enable: true
+full-node: false
+password: "swarm"
+swap-enable: false
+blockchain-rpc-endpoint: https://xdai.fairdatasociety.org
+verbosity: info
+mainnet: true`)
