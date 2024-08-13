22bb9413f9286de6931bac8c65a0753a87bcadd7
Cafe137 <xcafe137@gmail.com>
2024-08-13 23:00:27 +0200

    fix: quote asterisk

--- bee-yaml/index.js
+++ bee-yaml/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 console.log(`config: /etc/bee/bee.yaml
-cors-allowed-origins: *
+cors-allowed-origins: "*"
 data-dir: /var/lib/bee
 api-addr: 127.0.0.1:1633
 debug-api-addr: 127.0.0.1:1635
