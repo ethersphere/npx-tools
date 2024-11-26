b44e2d6ecebb089cbc07ce40455f40549c2dd716
Cafe137 <xcafe137@gmail.com>
2024-11-26 16:06:50 +0100

    feat: remove debug api from bee-yaml

--- bee-yaml/index.js
+++ bee-yaml/index.js
@@ -4,8 +4,6 @@ console.log(`config: /etc/bee/bee.yaml
 cors-allowed-origins: "*"
 data-dir: /var/lib/bee
 api-addr: 127.0.0.1:1633
-debug-api-addr: 127.0.0.1:1635
-debug-api-enable: true
 full-node: false
 password: "swarm"
 swap-enable: false
